1bcb4aed2f6d305ce73123397c1d7e740b1fd665
Vladimir <mr.qrsant@gmail.com>
2020-10-27 23:58:15 +0300

    Инициализация файлов, добавлен swiper с пагинацией и кнопками навигации

---
+++ src/js/app.js
@@ -0,0 +1,12 @@
+import Swiper from 'https://unpkg.com/swiper/swiper-bundle.esm.browser.min.js'
+
+var swiper = new Swiper('.swiper-container', {
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+});
